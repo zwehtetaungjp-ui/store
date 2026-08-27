d5dd87dbeb9a87e5cfc172cce10f5587f82774e8
zwehtetaungjp-ui <zwehtetaung.jp@gmail.com>
2026-08-27 14:41:00 +0900

    Update store.py

--- store.py
+++ store.py
@@ -10,7 +10,7 @@ st.set_page_config(
 )
 
 # Telegram Config
-BOT_TOKEN = "8803360365:AAHk_QUyHb5SBjMfzg_VyD9fqo5JENvkYI"
+BOT_TOKEN = "8403531874:AAGZjRK_4xPNZ5igmHRmu5NIuLf8rS1sb-g"
 CHAT_ID = "6826543956"
 
 # ပစ္စည်းများစာရင်း Data (၃၀ ခု)
